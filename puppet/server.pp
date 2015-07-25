0e61b2c26a2c421aeffdc051bf8e47b9ce26a855
root <root@dev.1community.my>
2015-07-25 16:51:51 -0400

    reorder serevr.pp

--- puppet/server.pp
+++ puppet/server.pp
@@ -1,65 +1,52 @@
 Exec { path => "/usr/bin:/usr/sbin:/bin:/sbin" }
 include ntp
-
-node default {
-
-	host { $::hostname:
-		ip => $::ipaddress_eth1,
-	}
-
-	class { 'resolv_conf':
-		nameservers => ['127.0.0.1','192.168.20.1'],
-	}
-
-	class { 'consul': 
-		# join_cluster => '172.20.20.10',
-		config_hash => {
-			'datacenter' => 'dc1',
-			'data_dir'   => '/opt/consul',
-			'log_level'  => 'INFO',
-			'node_name'  => $::hostname,
-#			'bind_addr'  => $::ipaddress_eth1,
-			'bind_addr'  => '0.0.0.0',
-			'bootstrap_expect' => 1,
-			'domain'     => 'consul.',
-			'recursor'   => '8.8.8.8',
-			'server'     => true,
-		}
-	}
-
-#        package{"dnsmasq": ensure => installed,}
-
-#        file{"/etc/dnsmasq.d/10-consul":
-#           ensure =>  'present',
-#           content => 'server=/consul/127.0.0.1#8600',
-#           notify  => Service['dnsmasq'],
-#           require => Package['dnsmasq'],
-#        }
-
-#        service{'dnsmasq': ensure => running, }
-
-	include dnsmasq
-	
-	dnsmasq::dnsserver { 'forward-zone-consul':
-		domain => "consul",
-		ip     => "127.0.0.1",
-		port   => "8600",
-	}
-
-	class { 'ambari_server':
-		repo => "http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.7.0/ambari.repo",
-	}
-
-	class { 'ambari_agent':
-		repo => "http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.7.0/ambari.repo",
-                serverhostname => $::hostname,
-	}
-
-	
-}
+include dnsmasq
 
 if $::puppetversion >= '3.6.1' {
   Package {
     allow_virtual => true,
   }
 }
+
+
+host { $::hostname:
+	ip => $::ipaddress_eth1,
+}
+
+dnsmasq::dnsserver { 'forward-zone-consul':
+	domain => "consul",
+	ip     => "127.0.0.1",
+	port   => "8600",
+}
+
+class { 'resolv_conf':
+	nameservers => ['127.0.0.1','192.168.20.1'],
+}
+
+class { 'consul': 
+	# join_cluster => '172.20.20.10',
+	config_hash => {
+		'datacenter' => 'dc1',
+		'data_dir'   => '/opt/consul',
+		'log_level'  => 'INFO',
+		'node_name'  => $::hostname,
+#		'bind_addr'  => $::ipaddress_eth1,
+		'bind_addr'  => '0.0.0.0',
+		'bootstrap_expect' => 1,
+		'domain'     => 'consul.',
+		'recursor'   => '8.8.8.8',
+		'server'     => true,
+	}
+}
+
+class { 'ambari_server':
+	repo => "http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.7.0/ambari.repo",
+}
+
+class { 'ambari_agent':
+	repo => "http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.7.0/ambari.repo",
+	serverhostname => $::hostname,
+}
+
+Class['dnsmasq'] -> Class['ntp'] -> Class['host'] -> Class['resolv_conf'] -> Class['consul'] -> Class['ambari-server'] -> Class['ambari_agent']
+
